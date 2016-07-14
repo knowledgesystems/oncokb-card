6cab1a8b89c467e5f7436772c7fb84a48aa1be0b
Hongxin Zhang <jackson.zhang.828@gmail.com>
2016-07-14 12:22:52 -0400

    Handle empty mutation effect/oncogenicity issue

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,71 +1,13 @@
 var OncoKBCard = (function(_, $) {
   var templateCache = {};
-  var meta = {
-    title: 'BRAF V600G in Melanoma',
-    oncogenicity: 'Oncogenic',
-    oncogenicityCitations: '1,2,4,5',
-    mutationEffect: 'Likely Switch-of-function',
-    mutationEffectCitations: '',
-    clinicalSummary: 'BRAF V600G has not been functionally or clinically validated. However, BRAF V600E/K [where E/K is the highest level] is known to be oncogenic [or likely oncogenic], and therefore this alteration is considered likely oncogenic. While the therapeutic implications are unknown for BRAF V600G, there are therapeutic implications for BRAF V600E/K in melanoma [where melanoma is the highest level].',
-    biologicalSummary: 'There is level 1 evidence that V600E or V600K mutations in BRAF is associated with response to FDA-approved combination of RAF-inhibitor dabrafenib and trametinib in melanoma (PMID: 25287827, 25399551, 25265492), as well as dabrafenib monotherapy in non-small cell lung cancer (Planchard, D et al. J Clin. Oncol. abstract 8009; 2013).',
-    treatments: [
-      {
-        level: '1',
-        variant: 'V600E',
-        treatment: 'Trametinib',
-        cancerType: 'Melanoma',
-        citations: '1,2,4,5'
-      },
-      {
-        level: '1',
-        variant: 'V600K',
-        treatment: 'Dabrafenib',
-        cancerType: 'Melanoma',
-        citations: '1,2,4,5'
-      },
-      {
-        level: '1',
-        variant: 'V600E/K',
-        treatment: 'Trametinib + Dabrafenib',
-        cancerType: 'Melanoma',
-        citations: '1,2,4,5'
-      },
-      {
-        level: '2A',
-        variant: 'V600E/K',
-        treatment: 'Trametinib + Dabrafenib',
-        cancerType: 'Melanoma/Non-Small Cell Lung Cancer',
-        citations: '1,2,4,5'
-      },
-      // {
-      //   level: '3A',
-      //   variant: 'V600E',
-      //   treatment: 'Vemurafenib',
-      //   cancerType: 'Melanoma',
-      //   citations: '1,2,4,5'
-      // },
-      // {
-      //   level: '3B',
-      //   variant: 'V600E/K',
-      //   treatment: 'Vemurafenib+Cobimetinib',
-      //   cancerType: 'Melanoma',
-      //   citations: ''
-      // },
-      // {
-      //   level: '4',
-      //   variant: 'V600E',
-      //   treatment: 'Cobimetinib',
-      //   cancerType: 'Melanoma',
-      //   citations: ''
-      // },
-      // {
-      //   level: 'R1',
-      //   variant: 'V600E',
-      //   treatment: 'Cobimetinib',
-      //   cancerType: 'Melanoma',
-      //   citations: '1,2,4,5'
-      // },
-    ]
+  var levelDes = {
+    '1': '<b>FDA-recognized</b> biomarker predictive of response to an <b>FDA-approved</b> drug <b>in this indication</b>',
+    '2A': '<b>Standard of care</b> biomarker predictive of response to an <b>FDA-approved</b> drug <b>in this indication</b>',
+    '2B': '<b>Standard of care</b> biomarker predictive of response to an <b>FDA-approved</b> drug <b>in another indication</b> but not standard of care for this indication',
+    '3A': '<b>Compelling clinical evidence</b> supports the biomarker as being predictive of response to a drug <b>in this indication</b> but neither biomarker and drug are standard of care',
+    '3B': '<b>Compelling clinical evidence</b> supports the biomarker as being predictive of response to a drug <b>in another indication</b> but neither biomarker and drug are standard of care',
+    '4': '<b>Compelling biological evidence</b> supports the biomarker as being predictive of response to a drug but neither biomarker and drug are standard of care',
+    'R1': '<b>Standard of care</b> biomarker predictive of <b>resistance</b> to an <b>FDA-approved</b> drug <b>in this indication</b>'
   };
 
   /**
@@ -99,38 +41,78 @@ var OncoKBCard = (function(_, $) {
   }
 
   function init(data, target) {
-    var treamtmentTemplates = [];
+    var treatmentTemplates = [];
+    var levelTemplates = [];
 
-    if(data) {
-      meta = data;
-    }
-    _.each(meta.treatments, function(treatment) {
+    _.each(data.treatments, function(treatment) {
       var treatmentFn = getTemplateFn("oncokb-card-treatment-row");
-      treamtmentTemplates.push(treatmentFn(treatment));
+
+      if(treatment.level){
+        treatment.levelDes = levelDes[treatment.level];
+      }
+      treatmentTemplates.push(treatmentFn(treatment));
     });
+
+    _.each(levelDes, function(levelDes, level) {
+      var levelFn = getTemplateFn("oncokb-card-level-list-item");
+      levelTemplates.push(levelFn({
+        level: level,
+        levelDes: levelDes
+      }));
+    });
+
     var cardMainTemplateFn = getTemplateFn("oncokb-card");
     var cardMainTemplate = cardMainTemplateFn({
-      title: meta.title,
-      oncogenicity: meta.oncogenicity,
-      oncogenicityCitations: meta.oncogenicityCitations,
-      mutationEffect: meta.mutationEffect,
-      mutationEffectCitations: meta.mutationEffectCitations,
-      clinicalSummary: meta.clinicalSummary,
-      biologicalSummary: meta.biologicalSummary,
-      treatmentRows: treamtmentTemplates.join('')
+      title: data.title,
+      oncogenicity: data.oncogenicity || 'Unknown to be oncogenic',
+      oncogenicityCitations: data.oncogenicityCitations,
+      mutationEffect: data.mutationEffect || 'Pending curation',
+      mutationEffectCitations: data.mutationEffectCitations,
+      clinicalSummary: data.clinicalSummary,
+      biologicalSummary: data.biologicalSummary,
+      treatmentRows: treatmentTemplates.join(''),
+      levelRows: levelTemplates.join('')
     });
 
     $(target).html(cardMainTemplate);
+
+    //Remove table element if there is no treatment available
+    if (!_.isArray(data.treatments) || data.treatments.length === 0) {
+      $(target + ' .oncogenic table').remove();
+    }
+
+    if(!data.oncogenicity) {
+      $(target + ' a.oncogenicity').addClass('grey-out');
+      $(target + ' a.oncogenicity').addClass('tab-disabled');
+    }
+
+    if(!data.mutationEffect) {
+      $(target + ' a.mutation-effect').addClass('grey-out');
+      $(target + ' a.mutation-effect').addClass('tab-disabled');
+    }
+
+    if(!data.biologicalSummary) {
+      $(target + ' #mutation-effect').remove();
+      $(target + ' a.mutation-effect').removeAttr('href');
+      $(target + ' a.oncogenic').removeAttr('href');
+      $(target + ' .enable-hover').each(function() {
+        $(this).removeClass('enable-hover');
+      });
+    }else {
+      $(target + ' .oncokb-card ul.tabs').tabs();
+    }
+
     $(target + ' .oncokb-card .collapsible').collapsible();
-    $(target + ' .oncokb-card ul.tabs').tabs();
 
     $(target + ' .oncokb-card .collapsible').on('click.collapse', '> li > .collapsible-header', function() {
       $(this).find('i.glyphicon-chevron-down').toggle();
       $(this).find('i.glyphicon-chevron-up').toggle();
     });
 
-    $(target + ' .oncokb-card i.fa-book').each(function() {
-      var content = $(this).attr('qtip-content');
+    // $(target + ' .oncokb-card i.fa-book').each(function() {
+    $(target + ' .oncokb-card [qtip-content]').each(function() {
+      var element = $(this);
+      var content = element.attr('qtip-content');
 
       if (content) {
         $(this).qtip({
@@ -150,8 +132,8 @@ var OncoKBCard = (function(_, $) {
             ready: false
           },
           position: {
-            my: 'center left',
-            at: 'center right',
+            my: element.attr('position-my') || 'center left',
+            at: element.attr('position-at') || 'center right',
             viewport: $(window)
           }
         });
@@ -159,6 +141,7 @@ var OncoKBCard = (function(_, $) {
         $(this).remove();
       }
     })
+
   }
 
   return {
